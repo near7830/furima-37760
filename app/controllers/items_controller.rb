62ed4584e4bfa114e0e75fde371630bb628ccf99
near7830 <near7830@gmail.com>
2022-04-27 00:24:11 +0900

    出品機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,30 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
+
   def index
+    @items = Item.includes(:user)
   end
 
   def new
     @item = Item.new
   end
 
-  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item, :name, :explain, :category_id, :sales_status_id, :prefecture_id, :shipping_fee_status_id, :scheduled_delivery_id, :price, :user_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,33 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :name,                   presence: true
-  validates :explain,                presence: true
-  validates :category_id,            presence: true, numericality:{ other_than: 1 , message: "can't be blank"}
-  validates :sales_status_id,        presence: true, numericality:{ other_than: 1 , message: "can't be blank"}
-  validates :shipping_fee_status_id, presence: true, numericality:{ other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id,          presence: true, numericality:{ other_than: 1 , message: "can't be blank"}
-  validates :scheduled_delivery_id,  presence: true, numericality:{ other_than: 1 , message: "can't be blank"}
-  validates :price,                  presence: true
+  belongs_to_active_hash :category
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :scheduled_delivery
+
+  with_options presence: true do
+    validates :image                  
+    validates :name                   
+    validates :explain             
+    validates :category_id
+    validates :sales_status_id         
+    validates :shipping_fee_status_id  
+    validates :prefecture_id   
+    validates :scheduled_delivery_id
+    validates :price,                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 , message: 'is invalid. Input half-width characters' }
+  end
+  
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :prefecture_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id 
+    validates :scheduled_delivery_id
+  end
+
   belongs_to :user
   has_one_attached :image
-  belongs_to_active_hash :category, :prefecture, :sales_status, :shipping_fee_status, :scheduled_delivery
-
+  
 end
