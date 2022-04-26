3708bf9b5b7dd8bb98433cee39be2949fff55e02
near7830 <near7830@gmail.com>
2022-04-26 14:08:32 +0900

    テーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  validates :name,                   presence: true
+  validates :explain,                presence: true
+  validates :category_id,            presence: true
+  validates :sales_status_id,        presence: true 
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :scheduled_delivery_id,  presence: true
+  validates :price,                  presence: true
+
+  belongs_to :user
+  has_one_attached :image
+
 end
