5ad94b292868598bdebb242be91d106443ea0f3f
near7830 <near7830@gmail.com>
2022-04-29 14:35:42 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, except: [:index, :new, :create ]
+  before_action :set_item, except: [:index, :new, :create]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id #&& @item.order.nil?
+    if @item.user_id == current_user.id # && @item.order.nil?
     else
       redirect_to root_path
     end
@@ -46,8 +46,6 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
-
-  
 
   private
 
