8265cd45c82214543e122f190066308ff586ca2b
near7830 <near7830@gmail.com>
2022-05-02 16:29:12 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :non_purchased_item, only: [:index, :create]
 
   def index
@@ -36,6 +36,6 @@ class OrdersController < ApplicationController
 
   def non_purchased_item
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
