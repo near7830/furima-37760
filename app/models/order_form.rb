f852a77f160f6fe472ed42f905d2113dcbf3d4bb
near7830 <near7830@gmail.com>
2022-05-02 15:45:27 +0900

    テストコード

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -9,12 +9,12 @@ class OrderForm
 
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
 
      #半角数値以外を入力したときのエラー
-    validates :phone_number, format: { with: /\A[0-9]\z/, message: 'is invalid. Input only numbert' }
+    validates :phone_number, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only numbert' }
 
      #文字数が足りなかったときのエラー表示
     validates :phone_number,length: {minimum: 10, maximum: 11, message: 'is too short' }
---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :order_form do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
+    postal_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { Faker::Address.city }
+    address { Faker::Address.street_address }
+    building { Faker::Address.street_address }
+    phone_number { '09036164910' }
+  end
+end
+
+#Faker::Number.decimal_part(digits: 11)
